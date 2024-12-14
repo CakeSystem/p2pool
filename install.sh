d4c314b0d402cfbf58cc463ae712fb16d77b50bd
CakeSystem <1625000148@qq.com>
2024-12-14 15:08:51 +0800

    4.2.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.1.5"
+VERSION="4.2.0"
 DOWNLOAD_HOST="https://github.com/CakeSystem/p2pool/raw/main/linux"
 ORIGIN_EXEC="p2pool-${VERSION}"
 
@@ -390,6 +390,7 @@ ISSUE() {
     echo "4.1.3"
     echo "4.1.4"
 	echo "4.1.5"
+	echo "4.2.0"
 }
 
 filterResult() {
